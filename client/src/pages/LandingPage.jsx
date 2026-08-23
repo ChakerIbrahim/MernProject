ab39da0c3d1024a8e83518342777e1765ca55566
Jalil <wasaya.jalil@gmail.com>
2026-08-23 21:05:07 +0300

    login page

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import Button from "../components/Button";
 import PageHeading from "../components/PageHeading";
 import { useAuth } from "../functions/authContext";
